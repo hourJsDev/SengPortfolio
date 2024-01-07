dbaaf0992223755612608e1207d98cf7334a1f4f
hourJsDev <145030272+hourJsDev@users.noreply.github.com>
2024-01-07 14:04:28 +0700

    responsive

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -5,14 +5,21 @@ const Home = () => {
   return (
     <section>
         <nav className='w-[90%] mx-auto mt-[1rem]'> 
-            <h1 className='text-[white]'>MINGSENG</h1>
+            <h1 className='text-[white] text-[1.3rem]'>MINGSENG</h1>
 
         </nav>
-        <div className="center font-bold uppercase text-center absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-[white]">
-            <h1 className='text-[7rem]'>darkness</h1>
-            <p className='text-[2.5rem]'>in the color of light</p>
+        <div className="center font-bold uppercase text-center absolute top-[40%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-[white]">
+            <h1 className='text-[7rem] max-md:text-[3rem]'>darkness</h1>
+            <p className='text-[2.5rem] max-md:text-[1rem]'>in the color of light</p>
         </div>
         <HomeCarousel/>
+        <div className='text-[white] max-md:text-[.6rem] max-md:top-[50%] max-md:-rotate-90 max-md:w-[20rem] max-md:-left-[8rem] uppercase text-[.8rem] items-center absolute top-[50%] -left-[10%] -rotate-90 flex w-[30rem] justify-between'>
+            <div className='flex flex-col items-center'>
+                <p>selected</p>
+                <p>works</p>
+            </div>
+            <p>@2023</p>
+        </div>
     </section>
   )
 }
--- src/Page/Home/HomeCarousel.jsx
+++ src/Page/Home/HomeCarousel.jsx
@@ -3,9 +3,9 @@ import { Carousel } from "antd";
 import { Link } from "react-router-dom";
 const contentStyle = {
   margin: 0,
-  height: "160px",
+  height: "100%",
   color: "#fff",
-  lineHeight: "160px",
+  lineHeight: "100%",
   textAlign: "center",
   background: "#364d79",
 };
@@ -13,27 +13,27 @@ const Images = [
   {
     id: 1,
     link: "",
-    src: "https://zandokh.com/image/catalog/banner/2024/1st-purchase-banner.jpg",
+    src: "https://scontent.fpnh1-1.fna.fbcdn.net/v/t39.30808-6/415511509_715601060546510_7596665559392325394_n.jpg?stp=cp0_dst-jpg_e15_q65_s320x320&_nc_cat=100&ccb=1-7&_nc_sid=ab7367&_nc_ohc=bQ5KNeLcEO0AX-hJHTK&_nc_ht=scontent.fpnh1-1.fna&oh=00_AfChVkgb1Dh2cDsHOCP0IFA8mywIX-GnCyTHEVMRjWAeXQ&oe=659FE4F7",
   },
   {
     id: 2,
     link: "",
-    src : "https://zandokh.com/image/catalog/banner/2023/fall-winter/APP9.jpg"
+    src : "https://scontent.fpnh1-2.fna.fbcdn.net/v/t39.30808-6/415972238_715601087213174_6410209589159271568_n.jpg?stp=cp0_dst-jpg_e15_q65_s320x320&_nc_cat=103&ccb=1-7&_nc_sid=ab7367&_nc_ohc=jD4ZoOj39XwAX-fis4T&_nc_ht=scontent.fpnh1-2.fna&oh=00_AfAc4j5IOAm25nrrUEsDENHuKsFzGkEeFxchCDUWf_y_Sw&oe=659EA897"
   },
 ];
 const HomeCarousel = () => {
   const onChange = (currentSlide) => {
-    console.log(currentSlide);
+    // console.log(currentSlide);
   };
   return (
-    <section className="mt-[70%] w-[50%] mx-auto">
-      <Carousel dots={"bg-black"} autoplay={false} autoplaySpeed={2000} afterChange={onChange}>
+    <section className="mt-[30%] max-md:w-[15rem] max-md:mt-[90%] h-[13rem] w-[25%] mx-auto">
+      <Carousel className="w-full" dots={"bg-black"} autoplay={false} autoplaySpeed={2000} afterChange={onChange}>
         {
           Images.map(i => {
             return (
               <Link key={i.id}>
                 <div style={contentStyle}>
-                  <img src={i.src} alt={"image"+i.id} />
+                  <img className="h-full w-full object-cover" src={i.src} alt={"image"+i.id} />
                 </div>
               </Link>
             )
